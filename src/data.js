8155060b90ef9b8d6b4d42f3bf2af31dbe585982
PRoy <sam469roger@gmail.com>
2023-03-25 08:31:16 +0530

    refactored tours component

--- src/data.js
+++ src/data.js
@@ -1,3 +1,8 @@
+import tour_1 from './images/tour-1.jpeg';
+import tour_2 from './images/tour-2.jpeg';
+import tour_3 from './images/tour-3.jpeg';
+import tour_4 from './images/tour-4.jpeg';
+
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
@@ -41,5 +46,48 @@ export const servicesInfo = [
     icon: 'fas fa-socks fa-fw',
     title: 'amazing comfort',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Asperiores, officia.',
+  },
+];
+
+export const toursInfo = [
+  {
+    id: 1,
+    img: tour_1,
+    date: 'august 26th, 2021',
+    title: 'Tibet Adventure',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'tibet',
+    days: '6 days',
+    price: 'from ₹2100',
+  },
+  {
+    id: 2,
+    img: tour_2,
+    date: 'october 1th, 2021',
+    title: 'best of java',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'indonesia',
+    days: '11 days',
+    price: 'from ₹1400',
+  },
+  {
+    id: 3,
+    img: tour_3,
+    date: 'september 15th, 2022',
+    title: 'explore hong kong',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'hong kong',
+    days: '8 days',
+    price: 'from ₹5000',
+  },
+  {
+    id: 4,
+    img: tour_4,
+    date: 'december 5th, 2022',
+    title: 'kenya highlights',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'kenya',
+    days: '20 days',
+    price: 'from ₹3300',
   },
 ];
